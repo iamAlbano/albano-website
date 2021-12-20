916990dcaefde47b8c83df1501cb2f392f37f7b7
Guilherme Albano <GuiAlbano10@gmail.com>
2021-12-20 19:15:34 -0300

    email update

--- albano/resources/views/email.blade.php
+++ albano/resources/views/email.blade.php
@@ -1,4 +1,7 @@
-<h2>Message from <strong>{{ $data['name']}}</strong></h2>
-<p>Good morning Guilherme! you have a new message.</p>
-<p>Sender: {{ $data['email']}}</p>
-<p>{{ $data['message'] }}</p>
+<div class="container">
+    <h2 class="h2">Message from <strong>{{ $data['name']}}</strong></h2>
+    <p>Good morning Guilherme! you have a new message.</p>
+    <p>Sender: {{ $data['email']}}</p>
+    <p>{{ $data['message'] }}</p>
+
+</div>
